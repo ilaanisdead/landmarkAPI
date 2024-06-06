cd786504daa37f8fad963c81938759832d41ceab
ilaan <benjailaan@gmail.com>
2024-06-06 12:43:02 +0300

    fifteenth commit

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from PIL import Image
 import tensorflow as tf
 import cv2
 from fastapi.middleware.cors import CORSMiddleware
-from keras.layers import TFSMLayer
+# from keras.layers import TFSMLayer
 from keras.models import Model
 
 app = FastAPI()
@@ -27,13 +27,13 @@ app.add_middleware(
 
 # MODEL = tf.keras.models.load_model("../models/1")
 # MODEL = tf.keras.models.load_model("./1.h5")
-# MODEL = tf.keras.models.load_model("./1")
+MODEL = tf.keras.models.load_model("./1")
 # MODEL = TFSMLayer("./1", call_endpoint='serving_default')
 
-input_layer = tf.keras.layers.Input(shape=(180, 180, 3))
-tfsm_layer = TFSMLayer("./1", call_endpoint='serving_default')
-output_layer = tfsm_layer(input_layer)
-MODEL = Model(inputs=input_layer, outputs=output_layer)
+# input_layer = tf.keras.layers.Input(shape=(180, 180, 3))
+# tfsm_layer = TFSMLayer("./1", call_endpoint='serving_default')
+# output_layer = tfsm_layer(input_layer)
+# MODEL = Model(inputs=input_layer, outputs=output_layer)
 
 CLASS_NAMES = ["Coast","Desert","Forest","Glacier","Mountain"]
 
@@ -60,7 +60,7 @@ async def predict(
     predictions = MODEL.predict(img_batch)
 
     predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = np.max(tf.nn.softmax(predictions   [0]))
+    confidence = np.max(tf.nn.softmax(predictions[0]))
 
     return {
         'class': predicted_class,
@@ -69,5 +69,6 @@ async def predict(
 
 if __name__ =="__main__":
     # MODEL.save('../modelsv5/1.h5')
-    print(app.version)
+    # print(app.version)
+    print(cv2.__version__)
     # uvicorn.run(app,host='localhost', port=8000)
