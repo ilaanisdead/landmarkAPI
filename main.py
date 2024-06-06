27a60b224e5a34590e241a2240d6ba6654e7ccce
ilaan <benjailaan@gmail.com>
2024-06-06 15:58:31 +0300

    twentieth commit

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import tensorflow as tf
 import cv2
 from fastapi.middleware.cors import CORSMiddleware
 import keras
+# import os
+
 # from keras.layers import TFSMLayer
 # from keras.models import Model
 
@@ -28,8 +30,12 @@ app.add_middleware(
 
 # MODEL = tf.keras.models.load_model("../models/1")
 # MODEL = tf.keras.models.load_model("./1.h5")
-# MODEL = tf.keras.models.load_model("./1")
-MODEL = keras.layers.TFSMLayer("./1.h5", call_endpoint='serving_default')
+# MODEL= tf.saved_model.load("./1.h5")
+MODEL = tf.keras.models.load_model("./1")
+# os.chdir("D:/campus/final.year.project.datasets/landmarkapi")
+
+# MODEL = keras.layers.TFSMLayer("1.h5", call_endpoint='serving_default')
+
 
 # input_layer = tf.keras.layers.Input(shape=(180, 180, 3))
 # tfsm_layer = TFSMLayer("./1", call_endpoint='serving_default')
@@ -72,4 +78,5 @@ if __name__ =="__main__":
     # MODEL.save('../modelsv5/1.h5')
     # print(app.version)
     # print(cv2.__version__)
+    # tf.saved_model.save(MODEL, f"./{2}")
     uvicorn.run(app,host='localhost', port=8000)
