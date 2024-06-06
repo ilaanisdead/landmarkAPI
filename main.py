54b4f3a98cb1ba2766b3cd9c6213297be3821d15
ilaan <benjailaan@gmail.com>
2024-06-06 13:37:10 +0300

    seventeenth commit

--- main.py
+++ main.py
@@ -70,5 +70,5 @@ async def predict(
 if __name__ =="__main__":
     # MODEL.save('../modelsv5/1.h5')
     # print(app.version)
-    print(cv2.__version__)
-    # uvicorn.run(app,host='localhost', port=8000)
+    # print(cv2.__version__)
+    uvicorn.run(app,host='localhost', port=8000)
